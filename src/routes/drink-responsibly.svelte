cf9f30c36cea0c2a5be4582aee1a7c196a8e40a7
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2022-05-25 03:51:07 -0400

    added legal pages, still needs formatting

---
+++ src/routes/drink-responsibly.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Nav from '$lib/navigation.svelte';
+	import Footer from '$lib/footer.svelte';
+</script>
+
+<Nav />
+<main>
+	<h2>terms & conditions</h2>
+	<h2>privacy policy</h2>
+	<h2>cookies policy</h2>
+	<p>
+		Legal Drinking Age Required: You must be of legal drinking age to access this Site. The Company
+		does not sell or serve alcohol to persons under the age of 21. By accessing the Site, you swear
+		and affirm that you are over the age of 21. The Company makes every effort to ensure that
+		alcoholic beverages are not delivered to or served to anyone who is under the age of 21. By
+		accessing this Site, you agree that any alcohol purchased from or received by you from
+		representatives of the Company is intended for personal consumption and not for resale. If you
+		do not agree with these conditions of use please do not use this Site. If you intentionally or
+		unintentionally misrepresent your age in order to provide alcohol to a person under the age of
+		21, the Company will prosecute you fully to the extent allowable by law. Government Warning
+		Concerning Health Consequences of Consuming Alcohol: By accessing this Site, you acknowledge
+		that there is a government warning concerning the health effects of consuming alcoholic
+		beverages: Government warning: (1) According to the Surgeon General, women should not drink
+		alcoholic beverages during pregnancy because of the risk of birth defects. (2) Consumption of
+		alcoholic beverages impairs your ability to drive a car or operate machinery, and may cause
+		health problems. Drink responsibly. Liability Waiver: In consideration for your being given
+		access to the Site, you hereby release, waive, discharge and covenant not to sue the Company,
+		subsidiaries, DBAs, affiliates, successors, contractors, agents, representatives and/or
+		employees from any and all liability, claims, demands, actions and causes of action whatsoever
+		arising out of or relating to any damage to your property or loss, damage or injury that you
+		personally sustain, including death, whether caused by the negligence of the Company or its
+		representatives or not while participating in Company programs, using Site content and/or
+		attending Company Events in person, regardless of location. You hereby assume all risks to your
+		property and your person and in no way will the Company be liable to you for damages or injuries
+		you sustain.
+	</p>
+</main>
+<Footer />
+
+<style>
+	main {
+		color: white;
+	}
+</style>
