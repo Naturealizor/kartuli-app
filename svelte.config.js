527eab5c82ebf83520bbe9eabe694ec571f1baf6
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2022-05-25 07:51:31 -0400

    added db file

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess.js';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
